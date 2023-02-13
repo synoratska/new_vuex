c7112e6c4343093ae52f3a8f653ef7f9b4d8a75b
synoratska <m.synoratska@gmail.com>
2023-02-13 13:17:26 +0200

    initial comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView'
+import Signup from '@/views/Signup'
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,13 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/signup',
+    name: 'signup',
+    component: Signup,
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Signup.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign Up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'login' }">
+              Need an account?
+            </router-link>
+          </p>
+          <ValidationErrors
+            v-if="validationErrors"
+            :validation-errors="validationErrors"
+          />
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="username"
+                v-model="username"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                type="email"
+                class="form-control form-control-lg"
+                placeholder="email"
+                v-model="email"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="password"
+                v-model="password"
+              />
+            </fieldset>
+
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ValidationErrors from '@/components/ValidationErrors.vue'
+export default {
+  name: 'SignupPage',
+  components: {
+    ValidationErrors,
+  },
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    }
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    },
+    validationErrors() {
+      return this.$store.state.auth.validationErrors
+    },
+  },
+  methods: {
+    onSubmit() {
+      console.log('Submitted')
+      this.$store
+        .dispatch('register', {
+          email: this.email,
+          username: this.username,
+          password: this.password,
+        })
+        .then((user) => {
+          console.log('Successfully registered user', user)
+          this.$router.push({ name: 'home' })
+        })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
