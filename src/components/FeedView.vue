8bbe688084fa55616a4fa090a823d1bfad70901f
synoratska <m.synoratska@gmail.com>
2023-02-27 13:52:35 +0200

    FeedView markup added

--- src/components/FeedView.vue
+++ src/components/FeedView.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>Feed {{ apiUrl }}</div>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, ind) in feed.articles"
+        :key="ind"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{
+              name: 'userProfile',
+              params: { slug: article.author.username },
+            }"
+          >
+            <img :src="article.author.image"
+          /></router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                user: 'userProfile',
+                params: { slug: article.author.username },
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+           ADD TO FAVORITES
+          </div>
+         </div>
+         <router-link :to="{name: 'article', params: {slug: article.slug}}" class="preview-link">
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+         </router-link>
+      </div>
+      PAGINATION
+    </div>
+  </div>
 </template>
 
 <script>
-import {actionTypes} from '@/store/modules/feed'
+import { mapState } from 'vuex'
+import { actionTypes } from '@/store/modules/feed'
 export default {
   name: 'FeedView',
   props: {
@@ -12,9 +57,16 @@ export default {
       required: true,
     },
   },
+  computed: {
+    ...mapState({
+      isLoading: (state) => state.feed.isLoading,
+      feed: (state) => state.feed.data,
+      error: (state) => state.feed.error,
+    }),
+  },
   mounted() {
-  this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
-  }
+    this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl })
+  },
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,24 +8,65 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/signup',
+    name: 'signup',
+    component: Signup,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
     path: '/',
     name: 'globalFeed',
     component: GlobalFeed,
   },
   {
-    path: '/signup',
-    name: 'signup',
-    component: Signup,
+    path: '/feed',
+    name: 'yourFeed',
+    component: GlobalFeed,
   },
-  { 
-    path:'/login',
-    name: 'login',
-    component: Login
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeed,
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeed,
+  },
+
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeed,
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeed,
   },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
